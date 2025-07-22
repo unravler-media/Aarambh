f3273d9113ced55df14cd22a05806f28d0597864
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-07-23 00:05:49 +0530

    Fixed the Typo of CONST

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -2,7 +2,7 @@
 // export const API_BASE_URL = 'https://aarambh.zeabur.app';
 
 // Backend Deployment Two
-export cosnt API_BASE_URL = "https://unr-c1.eu.org";
+export const API_BASE_URL = "https://unr-c1.eu.org";
 
 export const API_ENDPOINTS = {
   categories: '/api/category/',
