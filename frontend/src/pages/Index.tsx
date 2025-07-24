8c359b7caf7be54d386b674c5153a926de0ee64a
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-07-25 00:40:07 +0530

    updated homepage ui styling

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -53,9 +53,10 @@ const Index = () => {
       </div>
       
       <section className="mb-14">
-        <div className="flex items-center justify-between mb-6">
+
+      {/*<div className="flex items-center justify-between mb-6">
           <h1 className="text-2xl md:text-3xl font-bold text-white">Latest Posts</h1>
-        </div>
+        </div> */}
         
         <div className="grid grid-cols-2 sm:grid-cols-1 lg:grid-cols-2 gap-4">
           {postsToShow.map(post => (
