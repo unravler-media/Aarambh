964d558e78dd772deb54d8e8a57c14b871ecf8f3
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-07-27 00:03:59 +0530

    added www to the api

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -2,7 +2,7 @@
 // export const API_BASE_URL = 'https://aarambh.zeabur.app';
 
 // Backend Deployment Two
-export const API_BASE_URL = "https://unr-c1.eu.org";
+export const API_BASE_URL = "https://www.unr-c1.eu.org";
 
 export const API_ENDPOINTS = {
   categories: '/api/category/',
