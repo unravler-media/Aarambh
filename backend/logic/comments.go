ab6ddb9bfa0b2673e5d33ebbe6122df17285e895
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-07-27 01:54:25 +0530

    added delete comment logic

--- backend/logic/comments.go
+++ backend/logic/comments.go
@@ -54,5 +54,52 @@ func UpdateComment(c *fiber.Ctx) error {
 }
 
 func DeleteComment(c *fiber.Ctx) error {
-	return nil
+	db, ok := c.Locals("db").(*gorm.DB)
+	if !ok {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"response": "DB Fucked.",
+		})
+	}
+	session := c.Locals("session_user")
+	if session == nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"response": "Authorisation not provided.",
+		})
+	}
+	user_session := c.Locals("session_user").(*jwt.Token).Claims.(jwt.MapClaims)["sub"].(string)
+	comment_query := c.Query("id", "none")
+	if comment_query == "none" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"response": "Comment Query not provided",
+		})
+	}
+
+	var comment models.Comment
+	comnt_query := db.Where("id = ?", comment_query).First(&comment)
+	if comnt_query.Error != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"response": "Cannot get the comment",
+		})
+	}
+
+	if comnt_query.RowsAffected < 1 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"response": "Comment not Found",
+		})
+	}
+
+	if comment.AuthorID != user_session {
+		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"response": "Unauthorized Action.",
+		})
+	}
+
+	query := db.Delete(&comment)
+	if query.Error != nil {
+		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
+			"response": "Cannot Delete the comment.",
+		})
+	}
+
+	return c.SendStatus(200)
 }
