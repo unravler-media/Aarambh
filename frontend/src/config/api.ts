636d7c90d25ff795db3d3b46ae7b6623bcad2ddd
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-07-27 16:46:51 +0530

    added new deployment of Domain to frontend

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -2,7 +2,10 @@
 // export const API_BASE_URL = 'https://aarambh.zeabur.app';
 
 // Backend Deployment Two
-export const API_BASE_URL = "https://www.unr-c1.eu.org";
+// export const API_BASE_URL = "https://www.unr-c1.eu.org";
+
+// Backend Deployment Three
+export const API_BASE_URL = "https://aarambh-server-qco5mrkowq-em.a.run.app";
 
 export const API_ENDPOINTS = {
   categories: '/api/category/',
