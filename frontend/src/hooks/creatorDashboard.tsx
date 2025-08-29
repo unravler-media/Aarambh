9357c7e9b7140de1566c29a683fc99c26a05c7bc
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-08-29 11:15:10 +0530

    added Upload Image Logic =

--- frontend/src/hooks/creatorDashboard.tsx
+++ frontend/src/hooks/creatorDashboard.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { API_BASE_URL, API_ENDPOINTS } from "@/config/config";
+import { json } from "react-router-dom";
 
 export interface AddPostInterface {
   post_title: string;
@@ -182,5 +183,37 @@ export const deletePost = async (post_slug: string) => {
     return await response.json();
   } catch (err) {
     throw err
+  }
+}
+
+export const UploadImage = async (encoded: string, contentName: string, contentType: string) => {
+  try {
+    const authToken = localStorage.getItem("authToken");
+    const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.uploadImage}`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${authToken}`,
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        image: encoded,
+        name: contentName,
+        contentType: contentType
+      })
+    })
+    if (!response.ok) {
+      if (response.status === 400) {
+        console.log("unauthorised token")
+        localStorage.removeItem('user');
+        localStorage.removeItem('authToken');
+        window.location.href = "/";
+      } else {
+        throw new Error(`Unable to upload the image: ${await response.text()}`);
+      }
+    }
+    return await response.json()
+  } catch (err) {
+    throw err;
   }
 }
