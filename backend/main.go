ecc54e2ef339f6f9fc9e3b0f454556831c681668
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-09-03 00:59:13 +0530

    added new url

--- backend/main.go
+++ backend/main.go
@@ -43,7 +43,7 @@ func main() {
 	// CORS middleware
 	app.Use(cors.New(cors.Config{
 		AllowMethods:     "GET, POST, PUT, DELETE",
-		AllowOrigins:     "https://aarambh-one.vercel.app, http://127.0.0.1:3000, https://31f32cda-2994-4292-bed6-4a187ba6a181.lovableproject.com, http://localhost:4321",
+		AllowOrigins:     "https://aikyum.vercel.app, http://127.0.0.1:3000, https://31f32cda-2994-4292-bed6-4a187ba6a181.lovableproject.com, http://localhost:4321",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
 	}))
