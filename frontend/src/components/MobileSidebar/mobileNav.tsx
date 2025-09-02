280493885d8d043c82f5100788523fcd2f25568b
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-09-03 00:56:41 +0530

    changed project name and description

--- frontend/src/components/MobileSidebar/mobileNav.tsx
+++ frontend/src/components/MobileSidebar/mobileNav.tsx
@@ -38,9 +38,10 @@ const MobileNav = ({ categoriesData }: MobileNavProps) => {
 
   return (
     <div className="md:hidden">
-      <div className="fixed top-0 left-0 right-0 z-30 border-b border-[#1A1B22] px-4 py-3 flex justify-between items-center bg-[#090A0E]">
-        <a href="/" className="flex items-center space-x-2">
-          <h1 className="text-xl font-bold text-white">Aarambh.</h1>
+      <div className="fixed top-0 left-0 right-0 z-30 border-b border-[#1A1B22] px-4 py-4 flex justify-between items-center bg-[#090A0E]">
+        <a href="/" className="grid items-center space-x-2">
+          <h1 className="text-xl font-bold text-white">Aikyam.</h1>
+          <p className="text-gray-400 text-sm mt-1">The confluence of creation.</p>
         </a>
 
         <button onClick={toggleMenu} className="p-2 text-white">
