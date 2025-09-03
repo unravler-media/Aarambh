1fa6f02ec97556571fe30056bf29b4610d99594d
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-09-03 15:08:44 +0530

    updated somethings in sitemap.xml

--- frontend/src/pages/sitemap.xml.ts
+++ frontend/src/pages/sitemap.xml.ts
@@ -34,7 +34,7 @@ export const GET: APIRoute = async () => {
     .join("");
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+  <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${urls}
   </urlset>`;
 
