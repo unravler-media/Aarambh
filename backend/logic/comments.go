6f7c2e2af0c6f6ebf9f470144053a1ff6ed4bd2f
lonexd3vil <itzlonewolfzproductions@gmail.com>
2025-09-11 00:51:16 +0530

    updated comments with streamlines errors.

--- backend/logic/comments.go
+++ backend/logic/comments.go
@@ -1,8 +1,8 @@
 package logic
 
 import (
+	"backend/common"
 	"backend/models"
-	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
@@ -10,34 +10,25 @@ import (
 )
 
 func CreateComment(c *fiber.Ctx) error {
-	db, ok := c.Locals("db").(*gorm.DB)
-	if !ok {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"response": "DB Fucked.",
-		})
-	}
+	db, _ := c.Locals("db").(*gorm.DB)
 
 	user_session := c.Locals("session_user")
 	if user_session == nil {
-		c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"response": "Authrisation Not Provided.",
-		})
+		return common.UnauthorizedRequest(c, "Invalid Authrisation.")
 	}
 
 	user_id := user_session.(*jwt.Token).Claims.(jwt.MapClaims)["sub"].(string)
 	var comment models.Comment
+
 	if err := c.BodyParser(&comment); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"response": "Invalid Request Body.",
-		})
+		return common.InvalidRequest(c, "Invalid Request")
 	}
 
 	comment.AuthorID = user_id
 	save := db.Create(&comment)
+
 	if save.Error != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"response": "Unable to Save Query.",
-		})
+		return common.InternalServerError(c, "Cannot Save Comment.")
 	}
 
 	response := make(map[string]any)
@@ -45,32 +36,22 @@ func CreateComment(c *fiber.Ctx) error {
 	response["comment_text"] = comment.CommentText
 	response["updated_at"] = comment.UpdatedAt
 
-	return c.JSON(fiber.Map{
-		"response": response,
-	})
+	return common.Success(c, &response)
 }
 
 func UpdateComment(c *fiber.Ctx) error {
-	db, ok := c.Locals("db").(*gorm.DB)
-	if !ok {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"response": "DB Fucked.",
-		})
-	}
+	db, _ := c.Locals("db").(*gorm.DB)
 
 	session := c.Locals("session_user")
 	if session == nil {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"response": "Authorisation not provided.",
-		})
+		return common.UnauthorizedRequest(c, "Unauthorized.")
 	}
 
 	user_session := session.(*jwt.Token).Claims.(jwt.MapClaims)["sub"].(string)
-	comment_id := c.Query("comment_id","none")
+	comment_id := c.Query("comment_id", "none")
+
 	if comment_id == "none" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"response": "Comment ID Not Provided",
-		})
+		return common.InvalidRequest(c, "Invalid Comment ID")
 	}
 
 	type requestBody struct {
@@ -79,112 +60,83 @@ func UpdateComment(c *fiber.Ctx) error {
 
 	var req requestBody
 	var commentInstance models.Comment
+
 	query := db.First(&commentInstance, "id = ?", comment_id)
 
 	if err := c.BodyParser(&req); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"response": "Invalid Request Parameters.",
-		})
+		return common.InvalidRequest(c, "Invalid Request")
 	}
 
 	if query.Error != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"response": "Error finding the comment.",
-		})
+		return common.InternalServerError(c, "Error Updating Comment.")
 	}
 
 	if query.RowsAffected < 1 {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"response": "Comment not found.",
-		})
+		return common.NotFound(c, "Comment Not Found")
 	}
 
 	if commentInstance.AuthorID != user_session {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"response": "Unauthorized Action.",
-		})
+		return common.UnauthorizedRequest(c, "Unauthorized")
 	}
 
-//err := db.Debug().Model(&commentInstance).Updates(map[string]any{"comment_text": commentInstance.CommentText,}).Error
-	
 	commentInstance.CommentText = req.CommentText
 	commentInstance.AuthorID = user_session
 
 	err := db.Save(&commentInstance).Error
 	if err != nil {
-		fmt.Print(err)
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"response": "Unable to update comment.",
-		})
+		return common.InvalidRequest(c, "Cannot Update Comment.")
 	}
 
 	type responseStruct struct {
-		ID string `json:"id"`
-		UpdatedAt string `json:"updated_at"`
+		ID          string `json:"id"`
+		UpdatedAt   string `json:"updated_at"`
 		CommentText string `json:"comment_text"`
-		AuthorID string `json:"author_id"`
+		AuthorID    string `json:"author_id"`
 	}
 
-
-	var response responseStruct
-	response = responseStruct{
-		ID: commentInstance.ID,
-		UpdatedAt: commentInstance.UpdatedAt,
+	var response = responseStruct{
+		ID:          commentInstance.ID,
+		UpdatedAt:   commentInstance.UpdatedAt,
 		CommentText: commentInstance.CommentText,
-		AuthorID: commentInstance.AuthorID,
+		AuthorID:    commentInstance.AuthorID,
 	}
 
-	return c.JSON(fiber.Map{
-		"response": response,
-	})
+	return common.Success(c, &response)
 }
 
 func DeleteComment(c *fiber.Ctx) error {
-	db, ok := c.Locals("db").(*gorm.DB)
-	if !ok {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"response": "DB Fucked.",
-		})
-	}
+	db, _ := c.Locals("db").(*gorm.DB)
+
 	session := c.Locals("session_user")
 	if session == nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"response": "Authorisation not provided.",
-		})
+		return common.UnauthorizedRequest(c, "Unauthorized")
 	}
+
 	user_session := c.Locals("session_user").(*jwt.Token).Claims.(jwt.MapClaims)["sub"].(string)
 	comment_query := c.Query("id", "none")
+
 	if comment_query == "none" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"response": "Comment Query not provided",
-		})
+		return common.InvalidRequest(c, "Invalid Comment Query.")
 	}
 
 	var comment models.Comment
 	comnt_query := db.Where("id = ?", comment_query).First(&comment)
+
 	if comnt_query.Error != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"response": "Cannot get the comment",
-		})
+		return common.InvalidRequest(c, "Unable to Fetch Comment.")
 	}
 
 	if comnt_query.RowsAffected < 1 {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"response": "Comment not Found",
-		})
+		return common.NotFound(c, "Comment Does not Exist.")
 	}
 
 	if comment.AuthorID != user_session {
-		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"response": "Unauthorized Action.",
-		})
+		return common.UnauthorizedRequest(c, "Unauthorized")
 	}
 
 	query := db.Delete(&comment)
 	if query.Error != nil {
-		return c.Status(fiber.StatusBadGateway).JSON(fiber.Map{
-			"response": "Cannot Delete the comment.",
-		})
+		return common.BadGateway(c, "Cannot Delete Comment.")
 	}
 
 	return c.SendStatus(200)
